11010f307759490cc3c087cc3693f9ef2d00a8c0
kiquetal <kiquetal@gmail.com>
2025-05-06 13:01:30 -0300

    add main function to create pod and handle errors

--- codes/cmd/main.go
+++ codes/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	json_ "encoding/json"
 	"fmt"
 	"io"
 	corev1 "k8s.io/api/core/v1"
@@ -11,6 +12,15 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"net/http"
 )
+
+func main() {
+	err := createPod()
+	if err != nil {
+		fmt.Printf("Error Invoking createdPod : %v\n", err)
+	} else {
+		fmt.Println("Pod created successfully")
+	}
+}
 
 func createPod() error {
 	pod := createPodObject()
@@ -25,6 +35,12 @@ func createPod() error {
 	}
 	client := &http.Client{}
 	resp, err := client.Do(reqCreate)
+
+	if err != nil {
+		fmt.Printf("Error creating pod: %v\n", err)
+		return err
+	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
@@ -34,7 +50,7 @@ func createPod() error {
 		if err != nil {
 			return err
 		}
-		json, err := json.MarshalIndent(createdPod, "", "")
+		json, err := json_.MarshalIndent(createdPod, "", "")
 		if err != nil {
 			return err
 		}
@@ -44,7 +60,7 @@ func createPod() error {
 		if err != nil {
 			return err
 		}
-		json, err := json.MarshalIndent(status, "", "")
+		json, err := json_.MarshalIndent(status, "", "")
 		if err != nil {
 			return err
 		}
@@ -53,9 +69,34 @@ func createPod() error {
 	return nil
 }
 
+func deserializePodBody(serializer runtime.Serializer, body []byte) (*metav1.Status, error) {
+
+	var status metav1.Status
+	_, _, err := serializer.Decode(body, nil, &status)
+	if err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
+func deserializeStatusBody(serializer runtime.Serializer, body []byte) (*corev1.Pod, error) {
+	var pod corev1.Pod
+	_, _, err := serializer.Decode(body, nil, &pod)
+	if err != nil {
+		return nil, err
+	}
+	return &pod, nil
+}
+
 func buildPostRequest(body io.Reader) (*http.Request,
 	error) {
-	reqCreate, err := http.NewRequest("POST", "http://localhost/", body)
+	reqCreate, err := http.NewRequest("POST", "http://127.0.0.1:8080/api/v1/namespaces/default/pods", body)
+
+	if err != nil {
+		return nil, err
+	}
+	reqCreate.Header.Set("Accept", "application/json")
+
+	return reqCreate, nil
 
 }
 
